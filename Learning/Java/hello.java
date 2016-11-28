a8a07b1052f05e157af97c11a1e3a57aa450970f
Ben Wernsman <ben.wernsman@me.com>
2016-11-28 00:35:24 -0600

    Added Java
    -hello world
    -If statements

---
+++ Learning/Java/hello.java
@@ -0,0 +1,6 @@
+class Sandbox {
+	public static void main(String[] args) {
+		//print out to console
+		System.out.println("Hello World!");
+	}
+}
---
+++ Learning/Java/if.java
@@ -0,0 +1,22 @@
+class Sandbox {
+	public static void main(String[] args) {
+		//IF statement 
+		if(2 == 2){
+			System.out.println("True");
+		}
+		else{
+			System.out.println("False");
+		}
+		
+		int val = 10;
+		if(val < 10){
+			System.out.println("The value is smaller than 10");
+		}
+		else if(val == 10){
+			System.out.println("The value is equal to 10");
+		}
+		else{
+			System.out.println("The value is greater than 10");
+		}
+	}
+}
